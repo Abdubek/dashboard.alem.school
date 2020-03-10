31104ed273b57a8647a09f377d459bb4bcf5c6da
Ali Tlekbai <ali.tlekbai@gmail.com>
2020-03-10 14:01:13 +0600

    deadline

--- src/routes/admin/deadline/index.svelte
+++ src/routes/admin/deadline/index.svelte
@@ -48,7 +48,39 @@
 
     let checkedObjects = [];
     let projects = [];
-    let students = [];
+    let users = {};
+    let students = {};
+
+    let rules = {
+        mandatories: [
+            {
+                name: 'go_reloaded'
+            },
+            {
+                name: 'ascii_art'
+            },
+            {
+                name: 'ascii_art_web'
+            },
+            {
+                name: 'groupie_tracker'
+            },
+            {
+                name: 'lem_in'
+            }
+        ],
+        optionals: {
+            special: {
+                'groupie_tracker': 1
+            },
+            amount: 3
+        },
+        exams: {
+            minimum: 7,
+            amount: 2
+        }
+    };
+
     onMount(() => {
         customFetch(`${config.API_URL}/object/3357/children`).then(resp => {
             if (!resp.ok) {
@@ -82,32 +114,85 @@
             body: JSON.stringify(postBody),
         });
         resp = await resp.json();
-        const data = resp.data;
-        let users = data.users;
-        for (let i = 0; i < users.length; i++) {
-            for (var key in data) {
-                if (key === 'users') {
-                    continue;
-                }
-                let found = false;
-                for (let j = 0; j < data[key].length; j++) {
-                    if (users[i].user.id == data[key][j].user.id) {
-                        found = true;
-                        break;
-                    }
-                }
-                if (!found) {
-                    if (users[i].unfinished == null) {
-                        users[i].unfinished = [];
-                    }
-                    users[i].unfinished.push(key);
-                }
+        console.log(resp);
+        // iterate over users
+        const usersList = resp.data.users;
+        for (let i = 0; i < usersList.length; i++) {
+            const user = usersList[i].user;
+            users[user.id] = {...user};
+            users[user.id].finished = {};
+            users[user.id].deadline = {
+                mandatories: [],
+                optionals: rules.optionals,
+                exams: rules.exams
+            };
+        }
+        // iterate over projects
+        for (let key in resp.data) {
+            if (key == 'users') {continue;}
+            // iterate over those who finished the project
+            for (let i = 0; i < resp.data[key].length; i++) {
+                const userID = resp.data[key][i].user.id;
+                const projectType = resp.data[key][i].object.attrs;
+                console.log(userID);
+                users[userID].finished[key] = projectType;
             }
         }
-        users = users.filter(elem => {
-            return (elem.unfinished != null);
-        });
-        students = users;
+
+        // deadliners
+        let deadliners = [];
+        for (let userID in users) {
+            // check for mandatories
+            for (let i = 0; i < rules.mandatories.length; i++) {
+                let project = rules.mandatories[i];
+                if (!(project.name in users[userID].finished)) {
+                    users[userID].deadline.mandatories.push(project.name);
+                }
+            }
+            // optionals
+            let optionalsAmount = 0;
+            for (let optional in users[userID].deadline.optionals.special) {
+                for (let project in users[userID].finished) {
+                    if (users[userID].finished[project] == 'enrichment') {
+                        if (project.startsWith(optional)
+                            && users[userID].deadline.optionals.special[optional] > 0) {
+                            users[userID].deadline.optionals.special[optional]--;
+                        }
+                        optionalsAmount++;
+                    }
+                }
+            }
+            users[userID].deadline.optionals.amount -= optionalsAmount;
+            if (users[userID].deadline.optionals.amount < 0) {
+                users[userID].deadline.optionals.amount = 0;
+            }
+        }
+
+        // let users = data.users;
+        // for (let i = 0; i < users.length; i++) {
+        //     for (var key in data) {
+        //         if (key === 'users') {
+        //             continue;
+        //         }
+        //         let found = false;
+        //         for (let j = 0; j < data[key].length; j++) {
+        //             if (users[i].user.id == data[key][j].user.id) {
+        //                 found = true;
+        //                 break;
+        //             }
+        //         }
+        //         if (!found) {
+        //             if (users[i].unfinished == null) {
+        //                 users[i].unfinished = [];
+        //             }
+        //             users[i].unfinished.push(key);
+        //         }
+        //     }
+        // }
+        // users = users.filter(elem => {
+        //     return (elem.unfinished != null);
+        // });
+        // students = users;
     }
 </script>
 
